0fb164fd9566bce3a261ffcff73c64a02e288eee
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-03-21 12:21:22 +0200

    fix indentation in public_pages styles and login component files, email validation pattern moved to constants directory

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -2,33 +2,31 @@ import { Component } from '@angular/core';
 import { AngularTokenService } from 'angular-token';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { emailPattern } from '../constants';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.html',
+  templateUrl: './login.html'
 })
 
 export class Login {
+  emailPattern = emailPattern;
   signInUser:any = {};
-  emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   constructor(private tokenAuthSerivce: AngularTokenService,
               private router: Router,
               private toastr: ToastrService) { }
 
   onSignInSubmit() {
-    console.log(this.signInUser)
     this.tokenAuthSerivce.signIn(this.signInUser).subscribe((res) => {
-        this.toastr.success('Successfully logged in.');
-        this.router.navigate(['dashboard']);
-      },
-      (err) => {
-        if (err.error && err.error.errors) {
-          this.toastr.error(err.error.errors)
-        } else {
-          this.toastr.error('Whoops! Something went wrong...')
-        };
-      }
-    )
+      this.toastr.success('Successfully logged in.');
+      this.router.navigate(['dashboard']);
+    }, (err) => {
+      if (err.error && err.error.errors) {
+        this.toastr.error(err.error.errors);
+      } else {
+        this.toastr.error('Whoops! Something went wrong...');
+      };
+    });
   }
 }
