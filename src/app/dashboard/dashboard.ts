a14141181d49a742837ba503dd78f4b8a0b5142f
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-03-21 17:51:15 +0200

    Implement navigation bar and log out (#3)
    * Implement navigation bar and log out

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.html',
-  styleUrls: ['./dashboard.scss']
 })
 export class Dashboard {
 
