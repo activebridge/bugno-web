2b489317a2466d891bec742cc4b0055257435069
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-03-22 14:56:26 +0200

    Setup deploy

--- src/app/components/shared/navbar/navbar.ts
+++ src/app/components/shared/navbar/navbar.ts
@@ -8,6 +8,8 @@ import { Router } from '@angular/router';
 })
 
 export class Navbar {
+  isOpen: boolean;
+
   constructor(public tokenAuthSerivce: AngularTokenService,
               private router: Router) { }
 
