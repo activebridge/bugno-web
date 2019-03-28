7ce4a34b4f46c1d558072ed67c21ea1496e88c4a
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-03-28 16:17:28 +0200

    Delete project

--- src/app/components/project/project.ts
+++ src/app/components/project/project.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 import { ProjectAPI } from '../../api';
 
@@ -10,22 +11,54 @@ import { ProjectAPI } from '../../api';
 })
 
 export class Project implements OnInit {
+  projectId:number;
   project:any = {};
 
   constructor(private projectAPI: ProjectAPI,
-              private router: ActivatedRoute) { }
+              private router: ActivatedRoute,
+              private redirect: Router,
+              private toastr: ToastrService) { }
 
   ngOnInit() {
     this.router.params.subscribe(params => {
       if (params['id']) {
+        this.projectId = params['id'];
         this.getProject(params['id']);
       }
     });
   }
 
   getProject(id) {
-    this.projectAPI.get(id).subscribe((response:any) => {
-      this.project = response.data.attributes;
-    });
+    this.projectAPI.get(id).subscribe(this.onGetSuccess, this.onGetError);
+  }
+
+  deleteProject() {
+    this.projectAPI.delete(this.projectId)
+                   .subscribe(this.onDeleteSuccess, this.onDeleteError)
+  }
+
+  private onGetSuccess = (resp) => {
+    this.project = resp.data.attributes;
+  }
+
+  private onGetError = (error) => {
+    if (error.error.error) {
+      this.toastr.error(error.error.error);
+    } else {
+      this.toastr.error('Whoops! Something went wrong...');
+    }
+    this.redirect.navigate(['dashboard']);
+  }
+  private onDeleteSuccess = (resp) => {
+    this.toastr.success(`${this.project.name} was destroyed`);
+    this.redirect.navigate(['dashboard']);
+  }
+
+  private onDeleteError = (error) => {
+    if (error.error.error) {
+      this.toastr.error(error.error.error);
+    } else {
+      this.toastr.error('Whoops! Something went wrong...');
+    }
   }
 }
