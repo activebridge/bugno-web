d1278ed158d1aab1518e55f65728353c07770dee
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-04-25 11:30:37 +0300

    Show remove user from project button only to project owner

--- src/app/components/shared/member-list/member-list.ts
+++ src/app/components/shared/member-list/member-list.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AngularTokenService } from 'angular-token';
+import { find } from 'lodash';
 import { NotificationService } from '../../../utility/notification.service';
 
 import { ProjectUsersAPI } from '../../../api';
@@ -8,12 +10,24 @@ import { ProjectUsersAPI } from '../../../api';
   selector: 'app-member-list',
   templateUrl: './member-list.html',
 })
-export class MemberList {
+export class MemberList implements OnInit {
+  currentUser: any = {};
   @Input() projectUsers: any = [];
   @Input() projectId: string;
   constructor(private projectUsersAPI: ProjectUsersAPI,
+              private tokenService: AngularTokenService,
               private router: ActivatedRoute,
               private notifyService: NotificationService) { }
+
+  ngOnInit() {
+    this.tokenService.validateToken().subscribe( res => {
+      this.currentUser = this.tokenService.currentUserData;
+    });
+  }
+
+  get isCurrentUserOwner() {
+    return find(this.projectUsers, {attributes: { user_id: this.currentUser.id, role: 'owner'}});
+  }
 
   onDeleteProjectUser(projectUserId) {
     this.projectUsersAPI.delete(this.projectId, projectUserId).subscribe(
