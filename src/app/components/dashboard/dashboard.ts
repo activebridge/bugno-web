05911df2d3b41a5ea4ffa86cb665a908f55d6112
Dmitry Syvoglaz <grayeyed16@gmail.com>
2019-07-02 15:23:32 +0300

    Add project side bar to dashboard, change projest-list view

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.html',
+  styleUrls: ['./dashboard.scss']
 })
 
 export class Dashboard { }
