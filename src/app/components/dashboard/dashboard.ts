1ea2aa5ea68f5d7be01520dfd40e96669377447d
Dmitry Sivoglaz <grayeyed16@gmail.com>
2019-08-03 14:47:59 +0300

    Show event update activity

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.scss']
 })
 
-export class Dashboard { }
+export class Dashboard implements OnInit {
+  activities: any = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getActivities();
+  }
+
+  private getActivities() {
+    this.query().subscribe(
+      (resp) => {
+      this.activities = resp;
+      }),
+      (error) => {
+      console.log(error);
+      }
+  }
+
+  private query() {
+    return this.http.get(`${environment.apiEndpoint}/api/v1/activities`);
+  }
+}
